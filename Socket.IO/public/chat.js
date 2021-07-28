8da6b919143d65de508ef650d4c734b79bc6af5d
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-07-28 15:03:40 +0300

    WebSocket

---
+++ Socket.IO/public/chat.js
@@ -0,0 +1,9 @@
+const socket = io.connect('http://localhost:5000/')
+
+//socket.emit("sendMessageToServer");
+
+// socket.on("sendMessageToClient", (data) => {
+//   alert("sendMessageToClient tetiklendi."+data);
+// });
+
+socket.emit("name","murat")
---
+++ Socket.IO/server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const socket = require("socket.io")
+
+const app = express();
+
+
+
+app.use(express.static('public'))
+
+const port =process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı..`);
+  });
+const io = socket(server)
+
+io.on('connection',(socket)=>{
+    console.log("User connection :",socket.id)
+  
+    socket.on('disconnect',()=>{
+        console.log("User disconnect")
+    })
+
+    // socket.on("sendMessageToServer",()=>{
+    //     console.log("sendMessageToServer tetiklendi")
+    // })
+
+    // socket.emit("sendMessageToClient",23);
+
+    socket.on("name",data=>{
+        console.log("Adınız :"+data)
+    })
+  })
+
